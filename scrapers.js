fc332e984df789b67062136e3009580950f3593f
azure provisioned user <azureuser@datsy.datsy.d1.internal.cloudapp.net>
2013-11-23 23:09:36 +0000

    changed -08 to PST for timestamping

--- scrapers.js
+++ scrapers.js
@@ -54,7 +54,7 @@ var writeNewData = function(data) {
       + "lastcommunicationtime,"
       + "landmark"
       + ") VALUES (" +
-      "'" + row.executionTime + "-08'," + 
+      "'" + row.executionTime + " PST'," + 
       row.id + "," + 
       "'" + row.stationName + "'," + 
       row.availableDocks + "," + 
@@ -71,7 +71,7 @@ var writeNewData = function(data) {
       "'" + row.location + "'," + 
       "'" + row.altitude + "'," + 
       row.testStation + "," + 
-      "'" + row.lastCommunicationTime + "-08'," + 
+      "'" + row.lastCommunicationTime + " PST'," + 
       "'" + row.landMark + "'" +
       ");";
     db.insertNewData(query);
